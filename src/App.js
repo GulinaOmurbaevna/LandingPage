5284c75a1ddb197d1da9ac62d1b12a4ce6c3883e
GulinaOmurbaevna <gulinadev@gmail.com>
2024-07-20 17:59:37 +0600

    user

--- src/App.js
+++ src/App.js
@@ -10,6 +10,8 @@ function App() {
        <Navbar />
        <Hero />
        <User />
+   
+
     </div>
   );
 }
--- src/components/User.js
+++ src/components/User.js
@@ -1,10 +1,43 @@
 import React from 'react'
+import userLogo from '../images/Icon/user.png'
 
 function User() {
   return (
-    <div className='bg-white w-[1175px] h-[200px] shadow-xl rounded-sm border container'>
-      <div>
+    <div className='bg-white w-[83%] h-[200px] shadow-lg rounded-sm border container relative '>
+      <div className='flex justify-between px-5 h-full flex-wrap'>
+           <div className='flex gap-4 items-center'>
+            <div>
+                <img src={userLogo} alt="" />
+            </div>
 
+            <div >
+                <h3 className='font-bold text-[25px]'>90+</h3>
+                <p className='text-[#4f5665] my-2'>users</p>
+            </div>
+           </div>
+
+           <div className='flex gap-4 items-center'>
+            <div>
+                <img src={userLogo} alt="" />
+            </div>
+
+            <div >
+                <h3 className='font-bold text-[25px]'>90+</h3>
+                <p className='text-[#4f5665] my-2'>users</p>
+            </div>
+           </div>
+
+
+           <div className='flex gap-4 items-center'>
+            <div>
+                <img src={userLogo} alt="" />
+            </div>
+
+            <div >
+                <h3 className='font-bold text-[25px]'>90+</h3>
+                <p className='text-[#4f5665] my-2'>users</p>
+            </div>
+           </div>
       </div>
     </div>
   )
